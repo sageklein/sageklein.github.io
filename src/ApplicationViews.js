7c45b64b66968b7d61519d625cf619a854f5cc07
Sage Klein <sageklein@gmail.com>
2020-02-12 22:02:12 -0600

    added buttons and linked em on up

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -1,9 +1,6 @@
 import { Route } from "react-router-dom";
 import React, { Component } from "react";
-import Sidekick from "./components/Sidekick";
-import Bangazon from "./components/Bangazon";
-import Casaasa from "./components/Casaasa";
-import Hitpan from "./components/Hitpan";
+
 import Home from "./components/Home";
 
 export default class ApplicationViews extends Component {
@@ -16,38 +13,6 @@ export default class ApplicationViews extends Component {
 					path="/"
 					render={props => {
 						return <Home {...props} />;
-					}}
-				/>
-				<Route
-					exact
-					path="/casaasa"
-					render={props => {
-						return (
-							<Casaasa
-								{...props}
-							/>
-						);
-					}}
-				/>
-				<Route
-					exact
-					path="/bangazon"
-					render={props => {
-						return <Bangazon {...props} />;
-					}}
-				/>
-				<Route
-					exact
-					path="/sidekick"
-					render={props => {
-						return <Sidekick {...props} />;
-					}}
-				/>
-				<Route
-					exact
-					path="/hitpan"
-					render={props => {
-						return <Hitpan {...props} />;
 					}}
 				/>
 			</>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Header from "./Header";
-import { Link } from "react-router-dom";
 import Fade from "react-reveal/Fade";
 import data from "../Data";
 
@@ -11,23 +10,23 @@ class Main extends Component {
 			<div className="main-wraper">
 				<Header />
 				<div className="about-content">
-						<div className="about">
-					<h1>
-						<Fade bottom cascade>
-							About.
-						</Fade>
-					</h1>
+					<div className="about">
+						<h1>
+							<Fade bottom cascade>
+								About.
+							</Fade>
+						</h1>
 
-					<Fade bottom>
-						<div className="img">
-							<p>{data.abouttext}</p>
-							<img
-								src={require("../images/me.jpg")}
-								className="picture"
-								alt="Sage Klein"
-							/>
-						</div>
-					</Fade>
+						<Fade bottom>
+							<div className="img">
+								<p>{data.abouttext}</p>
+								<img
+									src={require("../images/me.jpg")}
+									className="picture"
+									alt="Sage Klein"
+								/>
+							</div>
+						</Fade>
 					</div>
 				</div>
 				<div className="work">
@@ -43,45 +42,160 @@ class Main extends Component {
 					<Fade bottom>
 						<div className="allProjects">
 							<div className="img_block">
-								<Link to="/bangazon">
-									<img
-										src={require("../images/Bangazon.png")}
-										className="img_link"
-										alt="Bangazon"
-									/>
-								</Link>
-								<h1>Consumer Platform Web-Application</h1>
+								<div className="header-title">
+									<a
+										className="header-title"
+										href="#Bangazon.png"
+										rel="noopener noreferrer"
+										target="_blank"
+									>
+										<img
+											src={require("../images/BangazonDashboard.png")}
+											className="img_link"
+											alt="Bangazon"
+										/>
+									</a>
+									<a
+										className="header-title"
+										href="https://www.figma.com/proto/Qbt3MbONrBQ9faxAKmXRHv/BANGAZON?node-id=28%3A760&viewport=355%2C471%2C0.09000614285469055&scaling=min-zoom"
+										rel="noopener noreferrer"
+										target="_blank"
+									>
+										<button className=" header-title">
+											View Prototype
+										</button>
+									</a>
+									<a
+										className="header-title, resume"
+										href="https://github.com/nss-day-cohort-35/bangazon/blob/master/README.md"
+										rel="noopener noreferrer"
+										target="_blank"
+									>
+										<button className="Case Study">
+											View Case Study
+										</button>
+									</a>
+								</div>
 							</div>
+						</div>
+					</Fade>
+					<Fade bottom>
+						<div className="allProjects">
 							<div className="img_block">
-								<Link to="/casaasa">
-									<img
-										src={require("../images/Casa.png")}
-										className="img_link"
-										alt="Casa Asafran"
-									/>
-								</Link>
-								<h1>Mentor Mentee Matching Application</h1>
+								<div className="header-title">
+									<a
+										className="header-title"
+										href="#Casa.png"
+										rel="noopener noreferrer"
+										target="_blank"
+									>
+										<img
+											src={require("../images/Casa.png")}
+											className="img_link"
+											alt="CasaAszafran"
+										/>
+									</a>
+									<a
+										className="header-title"
+										href="https://www.figma.com/proto/TJp9ggRNziv6yTAAmMjX6p/Casa-Asafran?node-id=22%3A0&viewport=379%2C385%2C0.12408243864774704&scaling=scale-down"
+										rel="noopener noreferrer"
+										target="_blank"
+									>
+										<button className=" header-title">
+											View Prototype
+										</button>
+									</a>
+									<a
+										className="header-title, resume"
+										href="https://docs.google.com/presentation/d/1xbWSW-3UcPYLpLtxNnBuErsDtr0GCbcVcapARPrF8ow/edit?usp=sharing"
+										rel="noopener noreferrer"
+										target="_blank"
+									>
+										<button className="Case Study">
+											View Case Study
+										</button>
+									</a>
+								</div>
 							</div>
+						</div>
+					</Fade>
+					<Fade bottom>
+						<div className="allProjects">
 							<div className="img_block">
-								<Link to="/hitpan">
-									<img
-										src={require("../images/Hitpan.png")}
-										className="img_link"
-										alt="Hit Pan"
-									/>
-								</Link>
-
-								<h1>Make-Up Organizing Application</h1>
+								<div className="header-title">
+									<a
+										className="header-title"
+										href="#Hitpan.png"
+										rel="noopener noreferrer"
+										target="_blank"
+									>
+										<img
+											src={require("../images/Hitpan.png")}
+											className="img_link"
+											alt="Hitpan"
+										/>
+									</a>
+									<a
+										className="header-title"
+										href="https://www.figma.com/proto/zktBnojjGWWSLoUR8YLjCv/Login?node-id=86183%3A1&viewport=770%2C306%2C0.20323914289474487&scaling=min-zoom"
+										rel="noopener noreferrer"
+										target="_blank"
+									>
+										<button className=" header-title">
+											View Prototype
+										</button>
+									</a>
+									<a
+										className="header-title, resume"
+										href="https://github.com/sageklein/Hit-Pan/blob/master/README.md"
+										rel="noopener noreferrer"
+										target="_blank"
+									>
+										<button className="Case Study">
+											View Case Study
+										</button>
+									</a>
+								</div>
 							</div>
+						</div>
+					</Fade>{" "}
+					<Fade bottom>
+						<div className="allProjects">
 							<div className="img_block">
-								<Link to="/sidekick">
-									<img
-										src={require("../images/Sidekick.png")}
-										className="img_link"
-										alt="Sidekick"
-									/>
-								</Link>
-								<h1>Behavioral Modification Web-Application</h1>
+								<div className="header-title">
+									<a
+										className="header-title"
+										href="#Hitpan.png"
+										rel="noopener noreferrer"
+										target="_blank"
+									>
+										<img
+											src={require("../images/Sidekick.png")}
+											className="img_link"
+											alt="Sidekick"
+										/>
+									</a>
+									<a
+										className="header-title"
+										href="https://www.figma.com/proto/wGq1mdLPiyRfw8lMtAtYOh/Sidekick-Persona?node-id=45%3A1&scaling=min-zoom"
+										rel="noopener noreferrer"
+										target="_blank"
+									>
+										<button className=" header-title">
+											View Prototype
+										</button>
+									</a>
+									<a
+										className="header-title, resume"
+										href="https://github.com/sageklein/Sidekick2.0/blob/master/README.md"
+										rel="noopener noreferrer"
+										target="_blank"
+									>
+										<button className="Case Study">
+											View Case Study
+										</button>
+									</a>
+								</div>
 							</div>
 						</div>
 					</Fade>
